4d46418c67599841047219628afcadc8849e137c
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-07-17 22:01:24 +0530

    implemented all api routes with orm

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from pyexpat import model
-from fastapi import FastAPI, HTTPException, status, Response
+from fastapi import FastAPI, HTTPException, status, Response, Depends
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from app.models.task import Task
 from .database import models
+from sqlalchemy.orm import Session
 from .database.connection import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -29,26 +29,25 @@ def root():
 
 
 @app.get("/tasks")
-def get_tasks():
-    cursor.execute("""SELECT * FROM tasks""")
-    tasks = cursor.fetchall()
+def get_tasks(db: Session = Depends(get_db)):
+    tasks = db.query(models.Task).all()
     return{"tasks": tasks, "count": len(tasks)}
 
 
 @app.post("/tasks/create", status_code=status.HTTP_201_CREATED)
-def create_task(task: Task):
-    cursor.execute("""INSERT INTO tasks (title, description) VALUES (%s, %s) RETURNING *""",
-                   (task.title, task.description))
-    new_task = cursor.fetchone()
-    con.commit()
+def create_task(task: Task, db: Session = Depends(get_db)):
+    new_task = models.Task(**task.dict())
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
     return {"task": new_task}
 
 
 @app.get("/tasks/{task_id}")
-def read_task(task_id: int):
-    cursor.execute("""SELECT * FROM tasks WHERE id = %s """,
-                   (str(task_id),))  # not working on double digit ids
-    task = cursor.fetchone()
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
     if not task:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"task with id {task_id} not found")
@@ -57,26 +56,28 @@ def read_task(task_id: int):
 
 
 @app.put("/tasks/{id}")
-def update_task(id: int, task: Task):
-    cursor.execute("""UPDATE tasks SET title = %s, description = %s, completed = %s WHERE id = %s RETURNING *""",
-                   (task.title, task.description, task.completed, str(id)))  # not working on double digit ids
-    updated_task = cursor.fetchone()
-    con.commit()
-    if not update_task:
+def update_task(id: int, task: Task,  db: Session = Depends(get_db)):
+
+    update_query = db.query(models.Task).filter(models.Task.id == id)
+
+    # if task does not exits
+    if not update_query.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"task with id {id} not found")
-
-    return {"data": updated_task}
+    # if task do exits
+    update_query.update(task.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": update_query.first()}
 
 
 @app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def deleteTask(id: int):
-    cursor.execute("""DELETE FROM tasks WHERE id = %s RETURNING *""",
-                   (str(id),))  # not working on double digit ids
-    deleted_task = cursor.fetchone()
-    con.commit()
-    if not deleted_task:
+def deleteTask(id: int,  db: Session = Depends(get_db)):
+
+    deleted_task = db.query(models.Task).filter(models.Task.id == id)
+    if not deleted_task.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"task with id {id} not found")
+    deleted_task.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
