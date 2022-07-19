85f83741f884f328cb7e58338194f59092751dbf
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-07-19 21:57:43 +0530

    migrated to environment variables

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/task-api-db"
+from ..config import env_settings
+SQLALCHEMY_DATABASE_URL = f"postgresql://{env_settings.database_username}:{env_settings.database_password}@{env_settings.database_hostname}/{env_settings.database_name}"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
--- app/utils/oauth2.py
+++ app/utils/oauth2.py
@@ -6,12 +6,13 @@ from ..database import models
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from ..config import env_settings
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = env_settings.secret_key
+ALGORITHM = env_settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = env_settings.token_expiry
 
 
 def create_access_token(data: dict):
